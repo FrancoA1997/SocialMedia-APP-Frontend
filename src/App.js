59c355428698ddda287c54afcdccd0a4eb1f49a9
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-02-13 19:50:17 -0300

    Dependencies  fix/material-ui-icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Home from "./pages/home/Home";
+import React from 'react';
+import { Person } from "@mui/icons-material";
 
 function App() {
-  return (
-    <div className="">
-     Hello
-    </div>
-  );
+
+  return <Person/>
+  
 }
 
 export default App;
